3fa3b25c8f0a1699cdac9d213a58fe2c732670c1
MD JASIM UDDIN <mjunachole97@gmail.com>
2024-05-07 01:16:17 +0600

    details route & craft post route added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://mdjasim97:jOb2CO9cH4IHadVf@cluster0.wukjrsy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,12 +35,7 @@ async function run() {
         const userCollection = database.collection("users")
         const craftCollection = database.collection("craft-items")
 
-        // all user find 
-        app.get("/", async(req, res) => {
-            const cursor = craftCollection.find()
-            const result = await cursor.toArray()
-            res.send(result)
-        })
+
 
         // all user find 
         app.get("/users", async(req, res) => {
@@ -53,6 +48,22 @@ async function run() {
         app.post("/users", async(req, res) => {
             const reqBody = req.body
             const result = await userCollection.insertOne(reqBody)
+            res.send(result)
+        })
+
+
+        // all craft item find 
+        app.get("/", async(req, res) => {
+            const cursor = craftCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // craft item find by id 
+        app.get("/craft/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await craftCollection.findOne(query)
             res.send(result)
         })
 
